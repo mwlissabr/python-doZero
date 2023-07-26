37a8f51d5f9d4ff09730b051762490ef411b2703
mwlissabr <mwlissabr@gmail.com>
2023-07-26 15:09:54 -0300

    novas entradas

--- python basico/aula20.py
+++ python basico/aula20.py
@@ -1,5 +1,13 @@
 # Enviar um email com os detalhes da compra online (máximo 3 tentativas)
 #para compras confirmadas
 
-compra_confirmada = True
-dados_
+compra_confirmada = False
+dados_compra = 'Compra no valor de R$12.50 e entrega confirmada!'
+
+for enviar in range(3):
+    if compra_confirmada:
+        print(dados_compra)
+        print('Detalhes enviados para o seu email.')
+        break
+else:
+        print('Falha na compra!')
---
+++ python basico/aula21.py
@@ -0,0 +1,11 @@
+## == for loop nestes ===
+
+    # Outer loop
+     # Inner loop
+
+
+
+for numero1 in range(1,6):
+    print('Produto ' + str(numero1))
+    for numero2 in range(11):
+        print(numero1, numero2)
